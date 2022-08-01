55c2144f0a5c08a2fe1381b8d31d58f201e22b2f
antonychristos83 <antonychristos83@gmail.com>
2022-08-01 11:17:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/spdemo/Sec.java
@@ -0,0 +1,28 @@
+package com.example.spdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sec extends AppCompatActivity {
+    EditText ed1,ed2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sec);
+        ed1=(EditText) findViewById(R.id.e1);
+        ed2=(EditText) findViewById(R.id.e2);
+        SharedPreferences sp;
+        sp=getSharedPreferences("SD", Context.MODE_PRIVATE);
+        ed1.setText(sp.getString("un","").toString());
+        ed2.setText(sp.getString("up","").toString());
+    }
+    public void onClose(View v){
+        finish();
+    }
+}
